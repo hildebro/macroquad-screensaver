d3d49e48fb49293533cfd9dd01e872b31b578186
Alexander Hildebrandt <alex@hilde.dev>
2022-11-28 11:55:00 +0100

    remove unused properties from game_state.rs

--- src/game_state.rs
+++ src/game_state.rs
@@ -1,39 +1,23 @@
-use macroquad::window::{screen_height, screen_width};
-
 use crate::game_instance::GameInstance;
 
 pub struct GameState {
     game_instances: Vec<GameInstance>,
-    window_width: f32,
-    window_height: f32,
 }
 
 impl GameState {
     pub fn new(game_instances: Vec<GameInstance>) -> Self {
-        GameState {
-            game_instances,
-            window_width: screen_width(),
-            window_height: screen_height(),
-        }
+        GameState { game_instances }
     }
 
     pub fn update(&mut self) {
         for game_instance in &mut self.game_instances {
             game_instance.update();
         }
-
-        self.update_absolute_size()
     }
 
     pub fn draw(&self) {
         for game_instance in &self.game_instances {
             game_instance.draw();
         }
-    }
-
-    /// Update width and height in case the user resizes the window.
-    fn update_absolute_size(&mut self) {
-        self.window_width = screen_width();
-        self.window_height = screen_height();
     }
 }
